6f5319bf14e0e299c1c1bf91d6e4534188bba2b4
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-05-01 23:36:28 +0700

    create custom header

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -1,32 +1,31 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>/css/header.css">
-<header>
-    <nav class="navbar navbar-expand-lg header-color mr-auto">
-    <a class="navbar-brand" href="#">Decorlite</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<link rel="stylesheet" href="<?php echo base_url('css/header.css'); ?>">
+<nav class="navbar navbar-expand-lg navbar-light mr-auto bg-custom-color">
+  <a class="navbar-brand" href="<?php echo base_url(); ?>">
+    <h1 class="display-6 fg-custom-color"> DECORLITE </h1>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Contact Us</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Daftar Keranjang</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Sign In</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Sign Up</a>
-        </li>
-        </ul>
-    </div>
-    </nav>
-</header>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item mx-2">
+        <a class="nav-link fg-custom-color" href="<?php echo base_url('page/about'); ?>">ABOUT</a>
+      </li>
+      <li class="nav-item mx-2">
+      <a class="nav-link fg-custom-color" href="<?php echo base_url('page/contact'); ?>">CONTACT</a>
+      </li>
+      <li class="nav-item mx-2">
+        <a class="nav-link fg-custom-color" href="<?php echo base_url('page/cart'); ?>">CART</a>
+      </li>
+      <li class="nav-item mx-2">
+        <a href="<?php echo base_url('page/signin'); ?>" class="btn btn-custom-color d-none d-lg-block">SIGN IN</a>
+        <a class="nav-link d-lg-none fg-custom-color" href="<?php echo base_url('page/signin'); ?>">SIGN IN</a>
+      </li>
+      <li class="nav-item mx-2">
+        <a href="<?php echo base_url('page/signup'); ?>" class="btn btn-custom-color d-none d-lg-block">SIGN UP</a>
+        <a class="nav-link d-lg-none fg-custom-color" href="<?php echo base_url('page/signup'); ?>">SIGN UP</a>
+      </li>
+    </ul>
+  </div>
+</nav>
