ebb3465a5f969c8331709dc5033c7b238b07031c
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-05-02 01:56:00 +0700

    add image for carousel

--- application/views/carousel/carousel.php
+++ application/views/carousel/carousel.php
@@ -6,13 +6,13 @@
     </ol>
     <div class="carousel-inner">
     <div class="carousel-item active">
-        <img class="d-block w-100" src="index.svg" alt="First slide">
+        <img class="d-block w-100" src="img/1663.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-        <img class="d-block w-100" src="index2.svg" alt="Second slide">
+        <img class="d-block w-100" src="img/5256.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-        <img class="d-block w-100" src="index3.svg" alt="Third slide">
+        <img class="d-block w-100" src="img/3305765.jpg" alt="Third slide">
     </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
