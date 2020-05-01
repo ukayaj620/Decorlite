0c5cfd78cf3ef81cee0af3a650b0eab8fce3cd27
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-05-02 01:07:04 +0700

    add sign up and sign in page

---
+++ application/views/credentials/signup.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="<?php echo base_url('css/credentials.css'); ?>">
+
+<div class="container-fluid">
+    <form class="custom-container">
+        <h1 class="inner-container display-6"> Let's Sign up! </h1>
+    <div class="form-group">
+        <label for="email">Name</label>
+        <input type="text" class="form-control" id="email" placeholder="name">
+    </div>
+    <div class="form-group">
+        <label for="password">Email</label>
+        <input type="text" class="form-control" id="password" placeholder="email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="text" class="form-control" id="password" placeholder="password">
+    </div>
+    <div class="form-group">
+        <label for="password">Confirm Password</label>
+        <input type="text" class="form-control" id="password" placeholder="confirm password">
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn-custom">Sign In</button>
+    </div>
+    </form>
+</div>
