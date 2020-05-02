6cff52ed0e68590a732fe924e10c1e9d9fa36d7f
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-05-02 22:33:22 +0700

    add about getting started header  on about

---
+++ application/views/about/aboutHeader.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="<?php echo base_url('css/aboutHeader.css'); ?>">  
+<div class="container-fluid">
+    <div class="about-text">
+        GETTING STARTED TO DECORLITE
+    </div>
+    <button class="custom-button">
+        Learn More
+    </button>
+</div>
