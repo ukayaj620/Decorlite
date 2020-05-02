02c13d07af138b0d172509ebac2519f4498866f6
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-05-02 17:17:33 +0700

    update carousel

--- application/views/carousel/carousel.php
+++ application/views/carousel/carousel.php
@@ -1,26 +1,21 @@
-<div id="carouselExampleIndicator" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
     <div class="carousel-item active">
-        <img class="d-block w-100" src="img/1663.jpg" alt="First slide">
+      <img src="img/1663.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-        <img class="d-block w-100" src="img/5256.jpg" alt="Second slide">
+      <img src="img/5256.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-        <img class="d-block w-100" src="img/3305765.jpg" alt="Third slide">
+      <img src="img/3305765.jpg" class="d-block w-100" alt="...">
     </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-    </a>
+  </a>
 </div>
