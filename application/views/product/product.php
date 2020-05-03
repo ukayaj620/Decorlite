330c58582f4e66a964617917a4aff9b6d09761b3
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-05-03 23:11:13 +0700

    add bit responsive to card items

--- application/views/product/product.php
+++ application/views/product/product.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="<?php echo base_url('css/product.css') ?>">
-<link rel="stylesheet" href="<?php echo base_url('css/items.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('css/card.css'); ?>">
 
 <div class="header-container">
     <h1 class="display-5">Latest Arrival Products</h1>
@@ -7,13 +7,13 @@
 </div>
 <div class="custom-container"> 
     <?php foreach ($items as $item) {?>
-        <div class="col-3 py-4">
+        <div class="col-md-3 py-4">
             <a href="<?php echo base_url(); ?>/">
-                <div class="card"">
-                    <img class="card-img-top" src="<?php echo $item->itemImage ?>" style="object-fit: cover;overflow: hidden;height: 16vw; width : 16vw;">
+                <div class="card">
+                    <img class="card-img-top custom-card" src="<?php echo $item->itemImage ?>">
                     <div class="card-body">
                         <h6 class="card-title text-dark text-center"><?php echo $item->itemName ?></h6>
-                        <nr />
+                        <br />
                         <h6 class="card-title text-dark text-center"><b>Rp. <?php echo $item->itemPrice ?>,00</b></h6>
                     </div>
                 </div>
