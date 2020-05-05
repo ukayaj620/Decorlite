52b99682d6dac7d1cb2ea7fe08531b568e31dc14
Realch <58062980+Realch@users.noreply.github.com>
2020-05-06 02:23:02 +0700

    Update aboutContent.php

--- application/views/about/aboutContent.php
+++ application/views/about/aboutContent.php
@@ -5,6 +5,11 @@
     Decorlite adalah toko online yang menjual barang dekorasi untuk
 	pesta. Kami hadir untuk membantu membuat pesta kalian menjadi lebih
 	meriah dan indah. Dari dekorasi pesta ulang tahun hingga dekorasi
-	pesta pernikahan.
+	pesta pernikahan kami telah sediakan. Kami juga menyediakan dekorasi 
+	yang sudah jadi atau masih dalam bentuk bahan dekorasi.
+	
+	Decorlite menjamin semua produk yang di beli pada situs Decorlite
+	ke tangan pembeli. Pesanan dapat langsung di kirim dengan cepat
+	ketika pembeli sudah melakukan pembayaran.
     </div>
 </div>
